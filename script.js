9f2da8ceda9d21a612926fa43c5a4453d84b11fe
DimasNugrohoRamadhan <yonkav28@gmail.com>
2022-12-06 21:34:00 +0700

    DataBase Film WARMINDVD done 20 list

---
+++ script.js
@@ -0,0 +1,205 @@
+//genre
+// Action, comedy, Horor, Romantic, Drama
+//...................===============>>>>>>>>>>>>>> WARMINDVD <<<<<<<<<<<<<=============...........................
+const DBfilm = [
+    {
+        id : 1,
+        title : 'The Peaky Blinders Season 5',
+        genre : 'Action',
+        harga: 92000,
+        rating: 9.7,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=Ruyl8_PT_y8',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 2,
+        title : 'Baby Driver',
+        genre : 'Action',
+        harga: 71000,
+        rating: 8.5,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=zTvJJnoWIPk',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 3,
+        title : 'Hot Fuzz',
+        genre : 'Comedy',
+        harga: 81000,
+        rating: 9.0,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=674Ka18uFuA',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 4,
+        title : 'John Wick',
+        genre : 'Action',
+        harga: 60500,
+        rating: 7.4,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=C0BMx-qxsP4',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 5,
+        title : 'Twenty Five Twenty One',
+        genre : 'Romantic',
+        harga: 55000,
+        rating: 8.6,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=n7F8o-SoK8s',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 6,
+        title : 'Scream',
+        genre : 'Horor',
+        harga: 45500,
+        rating: 6.3,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=beToTslH17s',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 7,
+        title : 'One Piece Film Red',
+        genre : 'Action',
+        harga: 105000,
+        rating: 7,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=89JWRYEIG-s',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 8,
+        title : 'Black Adam',
+        genre : 'Action',
+        harga: 63000,
+        rating: 7.5,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=X0tOpBuYasI',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 9,
+        title : 'Train To Busan',
+        genre : 'Horor',
+        harga: 54000,
+        rating: 7.6,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=1ovgxN2VWNc',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 10,
+        title : 'Crash Landing On You',
+        genre : 'Romantic',
+        harga: 79000,
+        rating: 8.7,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=GVQGWgeVc4k',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 11,
+        title : 'Miracle In Cell No 7',
+        genre : 'Drama',
+        harga: 81000,
+        rating: 8.2,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=G2wPoBy2JQI',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 12,
+        title : 'KKN Di Desa Penari',
+        genre : 'Horor',
+        harga: 67000,
+        rating: 7.6,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=_ykK5YsWes0',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 13,
+        title : 'Mariposa',
+        genre : 'Romantic',
+        harga: 73000,
+        rating: 8.1,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=N9PUbRIKYOA',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 14,
+        title : 'IMPERFECT',
+        genre : 'Drama',
+        harga: 62000,
+        rating: 7.8,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=f4Sc26vQHcU',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 15,
+        title : 'Mencuri Raden Saleh',
+        genre : 'Action',
+        harga: 76000,
+        rating: 8.2,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=DN3sRz_veBU',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 16,
+        title : 'Perahu Kertas',
+        genre : 'Drama',
+        harga: 43000,
+        rating: 6.1,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=vFjTDMJam30',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 17,
+        title : 'Nigth At The Museum',
+        genre : 'Comedy',
+        harga: 99000,
+        rating: 9.1,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=KMKk7Dn__-Y',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 18,
+        title : 'The Conjuring',
+        genre : 'Horor',
+        harga: 76000,
+        rating: 8.2,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=k10ETZ41q5o',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 19,
+        title : 'SPY',
+        genre : 'Comedy',
+        harga: 63000,
+        rating: 8.9,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=YrY3v1eDmQY',
+        review: [/*berisi object dari komentar/testimoni user */]
+    },
+    {
+        id : 20,
+        title : 'Cek Toko Sebelah',
+        genre : 'Comedy',
+        harga: 77000,
+        rating: 7.1,
+        cover: 'URL LINK COVER IMAGE',
+        trailer: 'https://www.youtube.com/watch?v=r9NJveLN3zI',
+        review: [/*berisi object dari komentar/testimoni user */]
+    }
+]
